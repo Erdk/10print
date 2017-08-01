895102202fef8a622f8022325cdd9aa4b01a0759
Lukas Redynk <mr.erdk@gmail.com>
2017-08-01 22:42:44 +0200

    Sleep and output 80x25
    * sleep every 500msec before printing (limits asciinema output size)
    * breakline after 80 chars, print 3 80x25 screens

--- 10print.c
+++ 10print.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main() { 
-  while(1) { printf("%s", drand48() < 0.5 ? "\e[1;31m/\e[0m" : "\e[1;32m\\\e[0m"); } 
+int main() {
+  int i = 0;
+  while(i++ < 6000) { 
+    printf("%s", drand48() < 0.5 ? "\e[1;31m/\e[0m" : "\e[1;32m\\\e[0m");
+    usleep(500);
+    if (!(i % 80)) printf("\n");
+  } 
 }
